e891b8125d5e43df6c9fdfce132a854fe2621790
Joshmedinag <71286714+Joshmedinag@users.noreply.github.com>
2026-08-16 18:06:17 -0600

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import os
-
-# OpenCV disables EXR I/O by default in many builds. Set it before cv2 is imported
-# by tests that create/read temporary EXR files.
-os.environ.setdefault("OPENCV_IO_ENABLE_OPENEXR", "1")
-os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
--- tests/test_reader_protocol.py
+++
@@ -1,6 +0,0 @@
-from aovguard.io.protocol import EXRReader
-
-
-def test_exr_reader_protocol_is_importable() -> None:
-    assert EXRReader.__name__ == "EXRReader"
-
